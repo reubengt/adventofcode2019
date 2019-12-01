9229245e88abb1dd6f8d33f64356e0e68fd066c5
reubengt <reubengeorgethomas@gmail.com>
2019-12-01 20:00:37 +0000

    add readme and convert input to separate txt file, parsing with node

---
+++ README.md
@@ -0,0 +1,15 @@
+# Advent of code 2019
+
+documented here are my solutions to advent of code 2019
+I had some fun doing this interactively in [twilioquest](https://twilio.com/quest), an interactive rpg that teaches you how to code.
+this repo can be run without the game, however, as a node project.
+
+# How to run
+
+The questions for each challenge are written as comments.
+i have set up an node project with dotenv to import the input files and parse them.
+
+- step 1: clone this repo
+- step 2: use node to run the file you'd like, for example  
+  `node day1.js`  
+   input files are saved as txt, and parsed to arrays using core node modules fs and path, and javascript's Array.split() method
--- day1.js
+++ day1.js
@@ -1,3 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+// First, get the contents of the input file for today's puzzle
+const inputFilePath = path.join(__dirname, "day1.txt");
+const inputFileContents = fs.readFileSync(inputFilePath, "utf-8");
+
+// Next, let's put every line in the input file into an array - the last line
+// will be blank, so we'll remove the last item in the array
+const input = inputFileContents.split("\n");
+input.pop();
+
 // The Elves quickly load you into a spacecraft and prepare to launch.
 
 // At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper. They haven't determined the amount of fuel required yet.
@@ -14,106 +26,7 @@
 
 // What is the sum of the fuel requirements for all of the modules on your spacecraft?
 
-const massArray = `90903
-135889
-120859
-137397
-56532
-92489
-87979
-149620
-137436
-62999
-71672
-61022
-139765
-69690
-109513
-67615
-77803
-146519
-96432
-129440
-67912
-143886
-126992
-129921
-122339
-61684
-149706
-52779
-106421
-145896
-145977
-76585
-136021
-63976
-97063
-114899
-118570
-102196
-129126
-98521
-136186
-68054
-72452
-92433
-145851
-98487
-149980
-114477
-125479
-62351
-131559
-115553
-129371
-147164
-83125
-146200
-68621
-55982
-79131
-64907
-95570
-132347
-76889
-96461
-123627
-128180
-113508
-70342
-139386
-131234
-140377
-119825
-80791
-52090
-62615
-101366
-138512
-113752
-77912
-64447
-146047
-94578
-82228
-116731
-123488
-103839
-120854
-54663
-129242
-51587
-149536
-71096
-110104
-145155
-139278
-78799
-62410
-64645
-112849
-60402`.split("\n");
+const massArray = input;
 console.log(massArray);
 
 const fuelCounterUpper = mass => {
